7c4bbe73c1570fbc36cd8273b5ce33f1ba9d12b0
omarLoco20 <140438250+omarLoco20@users.noreply.github.com>
2023-09-26 17:40:17 -0500

    misiles

---
+++ Assets/scriptsd/spawnerMisil.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnerMisil : MonoBehaviour
+{
+    public GameObject misil;
+   // public GameObject monedas;
+    public List<GameObject> listaOvnisYBasura;
+    GameObject enemy;
+
+    public Vector2 posicionPartida;
+    public float yMin;
+    public float yMax;
+
+    private void Awake()
+    {
+        // Inicializar la lista del pool
+        listaOvnisYBasura = new List<GameObject>();
+
+        // Crear los enemigos iniciales y agregarlas al pool
+        for (int i = 0; i < 20; i++)
+        {                      
+                enemy = Instantiate(misil);
+                      
+
+            enemy.SetActive(false); // Desactivar la bala para que no sea visible ni interaccione
+            listaOvnisYBasura.Add(enemy);
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("mandarEnemy", 0, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void mandarEnemy()
+    {
+
+        if (listaOvnisYBasura.Count > 0)
+        {
+
+            float randY = Random.Range(yMin, yMax);
+            posicionPartida.y = randY;
+
+            listaOvnisYBasura.RemoveAt(0);
+            listaOvnisYBasura[0].transform.position = posicionPartida;
+            listaOvnisYBasura[0].SetActive(true);
+
+        }
+
+
+    }
+
+    public void devolverEnemy(GameObject enemy)
+    {
+        enemy.SetActive(false);
+        listaOvnisYBasura.Add(enemy);
+
+    }
+}
